f665b2931cbb90bda47b52e3c6053bf00f72321e
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-26 04:40:25 +0530

    changed button color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,25 @@
-import { black } from 'color-name'
-import { PropTypes } from 'prop-types'
-import Button from './Button'
-const Header = ({title})  =>{
+import { black } from "color-name";
+import { PropTypes } from "prop-types";
+import Button from "./Button";
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 
-    const onClick = () => {
-        console.log('Click')
-    }
+Header.defaultProps = {
+  title: "Task Tracker",
+};
 
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
-        </header>
-    )
-}
-
-Header.defaultProps={
-    title:'Task Tracker',
-}
-
-Header.propTypes={
-    title:PropTypes.string.isRequired,
-}
-
-export default Header
+export default Header;
