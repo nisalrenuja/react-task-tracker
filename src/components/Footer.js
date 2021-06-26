7d9f39c4d38f8251724069c0435d71632f9d808d
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-26 05:34:09 +0530

    footer modofied

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2021 | All Rights Reserved</p>
+      <Link to="/about">Nisal Renuja Palliayguru</Link>
+    </footer>
+  );
+};
+
+export default Footer;
